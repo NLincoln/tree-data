40e2933d5aa8706da8d30c0624a57ee1760bacbf
Nathan <lincolnnathan205@gmail.com>
2019-12-29 00:21:53 -0600

    adds the benchmarks

---
+++ benches/btree_insertion.rs
@@ -0,0 +1,34 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use data::{BTree, Database, Disk};
+
+fn btree() -> (Database<impl Disk>, BTree) {
+    use std::io::Cursor;
+    let mut db = Database::initialize(Cursor::new(vec![])).unwrap();
+    let tree = BTree::init_btree_structure(&mut db).unwrap();
+    (db, tree)
+}
+
+fn btree_insert_n(n: u128) {
+    let (mut db, mut tree) = btree();
+    for key in 0..n {
+        tree.btree_insert(key, &[0, 1, 2, 3, 4], &mut db).unwrap();
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("btree_insert 10", |b| {
+        b.iter(|| btree_insert_n(10));
+    });
+    c.bench_function("btree_insert 100", |b| {
+        b.iter(|| btree_insert_n(100));
+    });
+    c.bench_function("btree_insert 1,000", |b| {
+        b.iter(|| btree_insert_n(1_000));
+    });
+    c.bench_function("btree_insert 10,000", |b| {
+        b.iter(|| btree_insert_n(10_000));
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
